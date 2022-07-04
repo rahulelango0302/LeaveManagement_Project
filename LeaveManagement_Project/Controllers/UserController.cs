d239dbe538c18913e3b4e3b67e7c63c5a55c4d29
rahulelango0302 <rahulelango1999@gmail.com>
2022-07-04 17:52:17 +0530

    Add project files.

---
+++ LeaveManagement_Project/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using LeaveManagement_Project.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeaveManagement_Project.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        const string uName="xyz";
+        [HttpPost]
+        public IActionResult Login(Login login)
+        {
+            if (login.UserName == "Raj" && login.Password == "ss")
+            {
+
+                 HttpContext.Session.SetString("uName", login.UserName);
+                return RedirectToAction("DashBoard", "User");
+            }
+            else
+            {
+                ViewBag.msg = "Invalid UserName/password";
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cancel()
+        {
+            return RedirectToAction("Index","Home");
+        }
+
+        public IActionResult DashBoard()
+        {
+            return View();
+        }
+    }
+}
